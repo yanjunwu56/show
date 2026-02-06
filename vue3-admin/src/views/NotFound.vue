5aceb0ab39c8db298bdd1c62c05e595d1b8c2a58
yanjunwu56 <35823429+yanjunwu56@users.noreply.github.com>
2026-02-06 17:56:37 +0800

    Revert "Vue3 react 后台项目"

--- vue3-admin/src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section class="not-found card">
-    <div class="card-title">Page not found</div>
-    <p class="card-subtitle">
-      The page you are trying to reach does not exist.
-    </p>
-    <RouterLink class="ghost-button" to="/dashboard">Go to dashboard</RouterLink>
-  </section>
-</template>
